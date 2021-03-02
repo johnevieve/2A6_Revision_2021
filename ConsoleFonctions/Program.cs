e9155c3b3a8c5edfa1283e31d5e3b61fb24c98df
genevieve trudel <genevievecote10@gmail.com>
2021-03-02 15:52:50 -0500

    Fin 4 Dossiers et fichiers

--- ConsoleFonctions/Program.cs
+++ ConsoleFonctions/Program.cs
@@ -6,7 +6,10 @@ namespace ConsoleFonctions
     {
         private static void Main()
         {
+            Console.Title = "*** Genevieve Trudel ***";
             Console.WriteLine("Bonjour, je suis Genevieve Trudel");
+            Console.WriteLine("Vive Git!");
+            Console.ReadKey();
         }
     }
 }
